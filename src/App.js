2a17f52a9451bcbe250e8f50d471f78382de8f11
Anish Dey <anishdey40@gmail.com>
2022-05-29 13:31:10 +0530

    Twice rendering issue fixed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import PreLoginView from "./widgets/preLoginView/preLoginView";
 import LandingPage from './widgets/landingView/landingPage';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import "./App.css";
 
 function App(props) {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path='/' element={<PreLoginView />} />
-          <Route exact path='/landingPage' element={<LandingPage />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route exact path='/' element={<PreLoginView />} />
+        <Route exact path='/landingPage' element={<LandingPage />} />
+      </Routes>
     </div>
   );
 }
